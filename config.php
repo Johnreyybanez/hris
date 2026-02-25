96630d7c1d638c9e19f836313f97cd022a398328
John Rey Ybanez <johnreyybanez@gmail.com>
2026-02-25 10:37:30 +0800

    Initial commit

--- config.php
+++
@@ -1,4 +0,0 @@
-<?php
-return [
-    'openai_key' => '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'
-];
